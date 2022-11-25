cf4be73e5d7d8779927140e7617f686ce9f0374c
Василь Медведев <medvedev@vasil>
2022-11-25 18:18:38 +0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # openvpn-ansible
-Ansible playbook for install and configure openvpn 
+Ansible OpenVPN playbook for install and configure openvpn 
+
+Simple playbook to install and reconfigure OpenVPN server, Add new client to "clients" var to create new profile config.
+If you want to revoke client's certificate just remove from "clients" var
+All profile configs are in "~/client-configs/files/*.ovpn" on your host
+add your remote host in inventory file and "remote_server" var
---
+++ roles/clients/files/gen_openvpn_profile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+OUTPUT_DIR=client-configs/files
+BASE_CONFIG=client-configs/base.conf
+ 
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    /etc/openvpn/server/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    /usr/share/easy-rsa/pki/issued/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    /usr/share/easy-rsa/pki/private/${1}.key \
+    <(echo -e '</key>\n<tls-crypt>') \
+    /etc/openvpn/server/ta.key \
+    <(echo -e '</tls-crypt>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
